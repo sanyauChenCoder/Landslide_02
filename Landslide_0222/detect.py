2d1577bea00d520101323f670cf16fe59a970adc
cc <chencongsanya@126.com>
2025-05-24 17:28:56 +0800

    Update detect.py

--- Landslide_0222/detect.py
+++ Landslide_0222/detect.py
@@ -2,7 +2,7 @@ import warnings
 warnings.filterwarnings('ignore')
 from ultralytics import RTDETR
 
-# 预测框粗细和颜色修改问题可看<使用说明.md>下方的<YOLOV8源码常见疑问解答小课堂>第六点
+
 
 if __name__ == '__main__':
     model = RTDETR('runs/train/exp/weights/best.pt') # select your model.pt path
