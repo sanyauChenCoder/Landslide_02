ea0689e639e7cff1c05c9fa3581e089a404e76b0
cc <chencongsanya@126.com>
2025-05-24 17:29:31 +0800

    Update export.py

--- Landslide_0222/export.py
+++ Landslide_0222/export.py
@@ -4,7 +4,6 @@ from ultralytics import RTDETR
 
 # onnx onnxsim onnxruntime onnxruntime-gpu
 
-# 导出参数官方详解链接：https://docs.ultralytics.com/modes/export/#usage-examples
 
 if __name__ == '__main__':
     model = RTDETR('rtdetr-weight-path')
