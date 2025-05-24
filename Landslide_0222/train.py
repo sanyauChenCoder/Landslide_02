01a0f92f044266ec8496e018ca63300b657be9cb
cc <chencongsanya@126.com>
2025-05-24 17:33:40 +0800

    Update train.py

--- Landslide_0222/train.py
+++ Landslide_0222/train.py
@@ -2,9 +2,7 @@ import warnings
 warnings.filterwarnings('ignore')
 from ultralytics import RTDETR
 
-# 指定显卡和多卡训练问题 统一都在<使用说明.md>下方常见错误和解决方案。
-# 整合多个创新点的B站视频链接:https://www.bilibili.com/video/BV15H4y1Y7a2/
-# 更多问题解答请看使用说明.md下方<常见疑问>
+
 
 if __name__ == '__main__':
     model = RTDETR('ultralytics/cfg/models/rt-detr/rtdetr-r18.yaml')
@@ -14,9 +12,9 @@ if __name__ == '__main__':
                 imgsz=640,
                 epochs=100,
                 batch=4,
-                workers=4, # Windows下出现莫名其妙卡主的情况可以尝试把workers设置为0
+                workers=4, 
                 # device='0',
-                # resume='', # last.pt path
+                # resume='', 
                 project='runs/train',
                 name='exp',
                 )
